a076ee0f272499f284bb7d8720a8999db371ba7f
Marcin Benke <marcin.benke@golem.network>
2021-05-11 10:14:57 +0200

    TH/Projections/Build2: stress newName

--- Code/TH/Projections/Build2.hs
+++ Code/TH/Projections/Build2.hs
@@ -6,7 +6,7 @@ build_p1 :: Q [Dec]
 build_p1 = do
   let p1 = mkName "p1"  
   a <- newName "a"
-  b <- newName "b"
+  b <- newName "a"
   return
     [ FunD p1 
              [ Clause [TupP [VarP a,VarP b]] (NormalB (VarE a)) []
